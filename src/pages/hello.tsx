e3b0aaa4f0eadb9bbd881dd056cc4b633a5dd907
Darío <dariokozicki@gmail.com>
2023-03-13 05:51:40 -0300

    fix warning

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import Wizard from "./components/wizard";
 
 const Hello: NextPage = () => {
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import Scene from "./components/scene";
 
 const Landing: NextPage = () => {
